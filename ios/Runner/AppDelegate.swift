e8e2c3b266a3090806fb67c6f8fa6e9b673e1a41
Denis <zertripfix@gmail.com>
2026-03-23 14:53:20 +0200

    feat: initialize Firebase for iOS and Android, update build.gradle.kts with dev SHA-1 key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,9 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Ключ обязательно должен быть ПЕРЕД GeneratedPluginRegistrant
+    GMSServices.provideAPIKey("AIzaSyAri8BUgkt8jIVDo6gRxjkMDtXg4xiGntc")
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
